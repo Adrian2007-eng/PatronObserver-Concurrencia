b3c7a732c2e6883762c48fa2df1c67f14bb41698
Adrian2007-eng <adrian.ledesma@usil.pe>
2026-06-16 11:03:28 -0500

    Add EventManager class for event handling

---
+++ src/observer/EventManager.java
@@ -0,0 +1,14 @@
+public class EventManager {
+
+    private Map<String,List<EventListener>> listeners;
+
+    private ExecutorService executor;
+
+    public void subscribe(...) { }
+
+    public void unsubscribe(...) { }
+
+    public void notify(...) { }
+
+    public void shutdown() { }
+}
