3fe6991b01ec5c9183849ddecf9e2028bfb4ffab
Adrian2007-eng <adrian.ledesma@usil.pe>
2026-06-16 15:21:18 -0500

    Add logging functionality to LoggingListener
    Implemented logging functionality in the update method to write messages to a specified log file.

--- src/observer/LoggingListener.java
+++ src/observer/LoggingListener.java
@@ -1,8 +1,38 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class LoggingListener
         implements EventListener {
 
+    private File log;
+    private String message;
+
+    public LoggingListener(
+            String logFilename,
+            String message) {
+
+        this.log = new File(logFilename);
+        this.message = message;
+    }
+
     @Override
-    public void update(String filename) {
-        ...
+    public void update(String filename)
+            throws IOException {
+
+        System.out.println(
+                "LoggingListener -> "
+                + Thread.currentThread().getName()
+        );
+
+        FileWriter escritor =
+                new FileWriter(log, true);
+
+        escritor.write(
+                message + ": "
+                + filename + "\n"
+        );
+
+        escritor.close();
     }
 }
