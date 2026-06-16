198c52bb41c4c10e2674fb41c33a11180bd240c7
Adrian2007-eng <adrian.ledesma@usil.pe>
2026-06-16 15:18:12 -0500

    Refactor main method with concurrency and event listeners
    Updated main method to include error handling and added event subscriptions for logging and email alerts.

--- src/observer/Application.java
+++ src/observer/Application.java
@@ -1,5 +1,37 @@
+import java.io.IOException;
+
 public class Application {
-    public static void main(String[] args) {
-        ...
+
+    public static void main(String[] args)
+            throws IOException, InterruptedException {
+
+        System.out.println("Observer con Concurrencia");
+
+        Editor editor = new Editor();
+
+        LoggingListener logger =
+                new LoggingListener(
+                        "log.txt",
+                        "Archivo actualizado"
+                );
+
+        EmailAlertsListener emailAlerts =
+                new EmailAlertsListener(
+                        "rjru@gmail.com",
+                        "Se realizó una modificación"
+                );
+
+        editor.events.subscribe("open", logger);
+        editor.events.subscribe("save", emailAlerts);
+
+        editor.openFile("doc.txt");
+        editor.saveFile();
+
+        editor.openFile("doc.txt");
+        editor.saveFile();
+
+        Thread.sleep(2000);
+
+        editor.events.shutdown();
     }
 }
