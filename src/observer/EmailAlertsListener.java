e5f5070fec146f8276e77d703b41315cbf6aa66e
Adrian2007-eng <adrian.ledesma@usil.pe>
2026-06-16 15:20:41 -0500

    Add email and message fields to EmailAlertsListener

--- src/observer/EmailAlertsListener.java
+++ src/observer/EmailAlertsListener.java
@@ -1,8 +1,30 @@
+import java.io.IOException;
+
 public class EmailAlertsListener
         implements EventListener {
 
+    private String email;
+    private String message;
+
+    public EmailAlertsListener(
+            String email,
+            String message) {
+
+        this.email = email;
+        this.message = message;
+    }
+
     @Override
-    public void update(String filename) {
-        ...
+    public void update(String filename)
+            throws IOException {
+
+        System.out.println(
+                "Email enviado a "
+                + email
+                + " sobre "
+                + filename
+                + " | "
+                + Thread.currentThread().getName()
+        );
     }
 }
