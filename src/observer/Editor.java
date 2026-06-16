ea82c99aaa71cb055cf58f115c296e617d71bb5a
Adrian2007-eng <adrian.ledesma@usil.pe>
2026-06-16 15:19:00 -0500

    Refactor Editor class to manage file operations
    Initialize EventManager and implement saveFile method.

--- src/observer/Editor.java
+++ src/observer/Editor.java
@@ -1,12 +1,39 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class Editor {
 
-    public EventManager events;
+    public EventManager events =
+            new EventManager();
+
+    private File file;
 
     public void openFile(String path) {
-        ...
+
+        this.file = new File(path);
+
+        events.notify(
+                "open",
+                this.file.getName()
+        );
     }
 
-    public void saveFile() {
-        ...
+    public void saveFile()
+            throws IOException {
+
+        FileWriter escritor =
+                new FileWriter(this.file);
+
+        escritor.write(
+                "Ya se acerca el fin del ciclo"
+        );
+
+        escritor.close();
+
+        events.notify(
+                "save",
+                this.file.getName()
+        );
     }
 }
